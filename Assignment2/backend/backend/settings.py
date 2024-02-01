4d5c9397f7802fefd4ecec4aa9032417e70f52ac
Junjunliu307 <156387844+Junjunliu307@users.noreply.github.com>
2024-02-01 00:09:56 -0800

    update

--- Assignment2/backend/backend/settings.py
+++ Assignment2/backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(by$z)=up5w%v5q%ht@i=fkyeofc2)mpygv%ddyrer3_sc+%$6'
+SECRET_KEY = 'django-insecure-9j6j40zy-_0bpe82&@$ltebok7jtvix@+5f_2bx6bnneiz%1=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
